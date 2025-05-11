1cc1f6f32c15837981f75a233b6e7743af9f7683
mervyn <minhdaihop@gmail.com>
2025-05-11 17:01:55 +0700

    Display User List From Firestore Database

--- app/src/main/java/com/example/chatapp_mobile/activities/MainActivity.java
+++ app/src/main/java/com/example/chatapp_mobile/activities/MainActivity.java
@@ -42,6 +42,8 @@ public class MainActivity extends AppCompatActivity {
 
     private void setListeners() {
         binding.imageSignOut.setOnClickListener(v -> signOut());
+        binding.fabNewChat.setOnClickListener(v ->
+                startActivity(new Intent(getApplicationContext(), UsersActivity.class)));
     }
     private void loadUserDetails() {
         binding.textName.setText(preferenceManager.getString(Constants.KEY_NAME));
@@ -65,7 +67,6 @@ public class MainActivity extends AppCompatActivity {
                         preferenceManager.getString(Constants.KEY_USER_ID)
                 );
         documentReference.update(Constants.KEY_FCM_TOKEN, token)
-                .addOnSuccessListener(unused -> showToast("Token updated successfully"))
                 .addOnFailureListener(e -> showToast("Unable to update token"));
     }
 
