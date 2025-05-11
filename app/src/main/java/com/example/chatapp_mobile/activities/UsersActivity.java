88d7f4712d0d3b378b619555493e178d83d63fda
mervyn <minhdaihop@gmail.com>
2025-05-11 17:02:16 +0700

    Display User List From Firestore Database

---
+++ app/src/main/java/com/example/chatapp_mobile/activities/UsersActivity.java
@@ -0,0 +1,90 @@
+package com.example.chatapp_mobile.activities;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.chatapp_mobile.R;
+import com.example.chatapp_mobile.adapters.UsersAdapter;
+import com.example.chatapp_mobile.databinding.ActivityUsersBinding;
+import com.example.chatapp_mobile.models.User;
+import com.example.chatapp_mobile.utilities.Constants;
+import com.example.chatapp_mobile.utilities.PreferenceManager;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UsersActivity extends AppCompatActivity {
+
+    private ActivityUsersBinding binding;
+    private PreferenceManager preferenceManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityUsersBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        preferenceManager = new PreferenceManager(getApplicationContext());
+        setListeners();
+        getUsers();
+    }
+
+
+    private void setListeners() {
+        binding.imageBack.setOnClickListener(v -> onBackPressed());
+    }
+
+    private void getUsers() {
+        loading(true);
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        database.collection(Constants.KEY_COLLECTION_USERS)
+                .get()
+                .addOnCompleteListener(task -> {
+                    loading(false);
+                    String currentUserId = preferenceManager.getString(Constants.KEY_USER_ID);
+                    if(task.isSuccessful() && task.getResult() != null) {
+                        List<User> users = new ArrayList<>();
+                        for(QueryDocumentSnapshot queryDocumentSnapshot : task.getResult()) {
+                            if(currentUserId.equals(queryDocumentSnapshot.getId())) {
+                                continue;
+                            }
+                            User user = new User();
+                            user.name = queryDocumentSnapshot.getString(Constants.KEY_NAME);
+                            user.email = queryDocumentSnapshot.getString(Constants.KEY_EMAIL);
+                            user.image = queryDocumentSnapshot.getString(Constants.KEY_IMAGE);
+                            user.token = queryDocumentSnapshot.getString(Constants.KEY_FCM_TOKEN);
+                            users.add(user);
+                        }
+                        if(users.size() > 0){
+                            UsersAdapter usersAdapter = new UsersAdapter(users);
+                            binding.usersRecycleView.setAdapter(usersAdapter);
+                            binding.usersRecycleView.setVisibility(View.VISIBLE);
+                        }else{
+                            showErrorMassage();
+                        }
+                    }else {
+                        showErrorMassage();
+                    }
+                });
+    }
+
+    private void showErrorMassage() {
+        binding.textErrorMassage.setText(String.format("%s", "No user available"));
+        binding.textErrorMassage.setVisibility(View.VISIBLE);
+    }
+
+    private void loading(Boolean isLoading){
+        if(isLoading){
+            binding.progressBar.setVisibility(View.VISIBLE);
+        }else{
+            binding.progressBar.setVisibility(View.INVISIBLE);
+        }
+    }
+}
