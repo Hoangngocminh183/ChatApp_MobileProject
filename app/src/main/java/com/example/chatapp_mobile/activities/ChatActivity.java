024174aaecfaddbbd1b7a896bab1fce3c475802d
mervyn <minhdaihop@gmail.com>
2025-05-12 00:46:46 +0700

    chat screen design

---
+++ app/src/main/java/com/example/chatapp_mobile/activities/ChatActivity.java
@@ -0,0 +1,20 @@
+package com.example.chatapp_mobile.activities;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.chatapp_mobile.R;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
